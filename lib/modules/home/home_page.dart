978ed8d5283a54a2fd5089ca998bcb6dd57dc0e0
EstevaoCalera <estevaoscalera@gmail.com>
2022-03-07 00:28:25 -0300

    Authentication process implemented and working!

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
 
+import '../../shared/themes/app_colors.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("HomePage"),
-      ),
+      appBar: PreferredSize(
+          preferredSize: const Size.fromHeight(152),
+          child: Container(
+            height: 152,
+            color: AppColors.primary,
+            child: Center(
+              child: ListTile(
+                title: const Text("Olá, Estêvão"),
+                subtitle: const Text("Mantenha suas contas em dia"),
+                trailing: Container(
+                  height: 48,
+                  width: 48,
+                  decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
